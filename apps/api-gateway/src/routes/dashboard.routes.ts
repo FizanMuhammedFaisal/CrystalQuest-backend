ab9819cd7078257c4aee7073df5a5eaa25cac189
Turbobot <turbobot@vercel.com>
2025-05-02 18:49:08 +0530

    feat(create-turbo): apply official-starter transform

--- apps/api-gateway/src/routes/dashboard.routes.ts
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-const router = express.Router();
-
-router.get('/hello', (req, res) => {
-  res.send({ message: 'Hello API thisis dahsboard' });
-});
-export const dashboardRouter = router;
