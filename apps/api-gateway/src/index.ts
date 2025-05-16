2811eff7cba492949269d436c06524391814f635
Fizan Muhammed Faisal <fizanmuhammedfaisal.k@gmail.com>
2025-05-16 14:52:16 +0530

    testing ci cd

--- apps/api-gateway/src/index.ts
+++ apps/api-gateway/src/index.ts
@@ -6,7 +6,7 @@ import { errorHandler } from './middlewares/error.middleware.js'
 import { standardResponse } from './middlewares/response.middleware.js'
 import cors from 'cors'
 import cookieParser from 'cookie-parser'
-
+// hey
 const host = process.env.HOST ?? 'localhost'
 
 const port = process.env.PORT ? Number(process.env.PORT) : 3001
